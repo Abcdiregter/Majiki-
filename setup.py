6c07931808f3ac07cd119f201fb1ef077e25791d
Abcdiregter <111136892+Abcdiregter@users.noreply.github.com>
2023-03-19 15:44:22 +0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+import sys
+
+try: 
+os.system('pip install colorama')
+os.system('pip install requests')
+os.system('pip install getpass')
+os.system('pip install socket')
+os.system('pip install time')
+
+
+print ("""
+
+• ▌ ▄ ·.  ▄▄▄·  ▐▄▄▄▪  ▄ •▄ ▪    .▄▄ · ▄▄▄ .▄▄▄▄▄▄• ▄▌ ▄▄▄·
+·██ ▐███▪▐█ ▀█   ·████ █▌▄▌▪██   ▐█ ▀. ▀▄.▀·•██  █▪██▌▐█ ▄█
+▐█ ▌▐▌▐█·▄█▀▀█ ▪▄ ██▐█·▐▀▀▄·▐█·  ▄▀▀▀█▄▐▀▀▪▄ ▐█.▪█▌▐█▌ ██▀·
+██ ██▌▐█▌▐█ ▪▐▌▐▌▐█▌▐█▌▐█.█▌▐█▌  ▐█▄▪▐█▐█▄▄▌ ▐█▌·▐█▄█▌▐█▪·•
+▀▀  █▪▀▀▀ ▀  ▀  ▀▀▀•▀▀▀·▀  ▀▀▀▀   ▀▀▀▀  ▀▀▀  ▀▀▀  ▀▀▀ .▀   
+
+""")
